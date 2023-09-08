8b3a2a231157445f1675a46e0b4ef8200ad25cf1
Antonio Gallardo Girela <gallardogirela@gmail.com>
2023-09-08 20:56:49 +0200

    fix(Header): change Header position

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,13 +15,13 @@ const Header = () => {
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border 
         border-cyan-800/40 bg-cyan-800/40 shadow-lg shadow-cyan-950/[0.2]
-        dark:shadow-white/[0.2] backdrop-blur-[0.5rem] sm:top-3 sm:h-[3.25rem] 
+        dark:shadow-white/[0.2] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] 
         sm:w-[45rem] sm:rounded-full dark:bg-white/30 dark:border-white/30"
         initial={{ y: -100, x: '-50%', opacity: 0 }}
         animate={{ y: 0, x: '-50%', opacity: 1 }}
         transition={{ duration: 1.5 }}
       ></motion.div>
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[0.9rem] sm:h-[initial] sm:py-0">
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-cyan-950 sm:w-[initial] sm:flex-nowrap sm:gap-3">
           {links.map((link) => (
             <motion.li
