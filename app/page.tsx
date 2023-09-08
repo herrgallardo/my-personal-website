ef9da90da4b1c33c7c1b7bb054a50755f227cc38
Antonio Gallardo Girela <gallardogirela@gmail.com>
2023-09-08 04:14:39 +0200

    task: add components to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import About from '@/components/About'
+import Contact from '@/components/Contact'
+import Education from '@/components/Education'
+import Experience from '@/components/Experience'
+import Intro from '@/components/Intro'
+import Languages from '@/components/Languages'
+import Skills from '@/components/Skills'
+
 const Home = () => {
-  return <div></div>
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <About />
+      <Skills />
+      <Experience />
+      <Education />
+      <Languages />
+      <Contact />
+    </main>
+  )
 }
 
 export default Home
