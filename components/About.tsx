04268dfccfd5fd44b9b42ce8eadb3597a104e93c
Antonio Gallardo Girela <gallardogirela@gmail.com>
2023-11-20 11:56:20 +0100

    Update "About me" section for clarity and focus

--- components/About.tsx
+++ components/About.tsx
@@ -21,48 +21,28 @@ export default function About() {
         <SectionHeading>About me</SectionHeading>
         <div className="mb-3">
           <p className="my-5">
-            Born with an innate fascination for languages and cultures, I
-            embarked on a journey that led me to acquire a degree in Spanish and
-            Linguistics from Lund University. My time at university provided me
-            with a profound understanding of language structures, which later
-            served as a foundation when I transitioned into the world of coding
-            and web development.
+            Born with a deep interest in languages and cultures, I completed a
+            degree in Spanish and Linguistics at Lund University. This education
+            gave me an understanding of language structures, which later helped
+            me transition into coding and web development. I pursued a
+            Full-stack Development program at Teknikhögskolan i Lund, developing
+            skills in JavaScript, TypeScript, React, Next.js, HTML, CSS,
+            GraphQL, Git, and Jira.
           </p>
           <p className="my-5">
-            My passion for understanding systems and structures naturally segued
-            into a love for coding, allowing me to draw parallels between the
-            intricacies of languages and the logic of programming. In pursuit of
-            this newfound interest, I dedicated myself to a two-year program in
-            Full-stack Development at Teknikhögskolan i Lund.
+            My journey has been diverse, with work experiences in Granada,
+            London, and Lund, including managerial roles and customer service.
+            I&apos;ve also held positions such as a Spanish language instructor
+            and have extensive experience in the service industry. These roles
+            have equipped me with collaboration, leadership, and resilience
+            skills.
           </p>
           <p className="my-5">
-            The experience broadened my horizons, equipping me with skills in
-            JavaScript, TypeScript, React, Next.js, HTML, CSS, GraphQL, Git, and
-            Jira. The challenges of coding never cease to captivate me, and the
-            thrill of problem-solving in this domain has been deeply rewarding.
-          </p>
-          <p className="my-5">
-            But my journey is not just limited to the confines of academics and
-            programming. Over the years, I&apos;ve worn various hats, from
-            diverse jobs in the cities of Granada and London to leading Spanish
-            language sessions in Lund. My diverse work experiences, ranging from
-            managerial roles in Spain to customer service in London, have
-            ingrained in me a unique blend of soft skills and adaptability.
-          </p>
-          <p className="my-5">
-            Whether managing a tavern in Granada or undertaking voluntary
-            services in Eksjö, every experience has imparted lessons in
-            collaboration, leadership, and resilience.
-          </p>
-          <p className="my-5">
-            After successful internships and summer jobs in companies like Axis
-            Communications and 24HR, and a decade-long tenure in the service
-            industry, I am now eager to channel my passion for coding into a
-            full-time front-end developer position. With a rich tapestry of
-            experiences under my belt and a keen analytical mind sharpened by
-            linguistic studies and coding challenges, I am excited to contribute
-            to a team that values both technical prowess and a multidimensional
-            perspective.
+            After internships and jobs at Axis Communications and 24HR, I am now
+            looking for a full-time front-end developer role. I bring a unique
+            blend of linguistic knowledge, coding skills, and a wide range of
+            soft skills, eager to contribute to a team that values technical
+            ability and diverse experiences.
           </p>
         </div>
       </motion.section>
